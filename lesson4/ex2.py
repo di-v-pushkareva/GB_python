43b6911e905788047505fc3fc9d13eda57bb5c84
diana <dianacvet9645@gmail.com>
2022-05-29 12:06:23 +0300

    Урок 4
    ДЗ к Уроку 4

---
+++ lesson4/ex2.py
@@ -0,0 +1,4 @@
+my_list = [34, 12, 5, 5, 7, 8, 4, 10]
+my_new_list = [el for num, el in enumerate(my_list) if my_list[num - 1] < my_list[num]]
+print(f'Исходный список {my_list}')
+print(f'Новый список {my_new_list}')
